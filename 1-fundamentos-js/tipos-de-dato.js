7239134a7c7631c110bc183d60257645b7563bdc
Dan Gomez <gomezmhector05@gmail.com>
2025-02-09 18:23:50 -0600

    actividad1 tipos de datos en js

--- 1-fundamentos-js/tipos-de-dato.js
+++ 1-fundamentos-js/tipos-de-dato.js
@@ -1,1 +1,16 @@
-
+console.log(typeof 42); //number
+console.log(typeof 'Veinticinco'); //string
+console.log(typeof -666); //number
+console.log(typeof true); //boolean
+console.log(typeof 0); //number
+console.log(typeof ''); //number
+console.log(typeof null); //special Null
+console.log(typeof undefined); //special Undefined
+console.log(typeof FALSE); //undefined
+console.log(typeof 42); //number
+let simbolo = Symbol('id');
+console.log(typeof simbolo); //symbol );
+let objeto = {nombre: "Hector", edad: 24}; //object
+console.log(typeof objeto);
+let bigNumber = BigInt(12345678901234567); //bigint
+console.log(typeof bigNumber);
